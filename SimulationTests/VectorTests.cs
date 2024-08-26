a907fb0fac95593b7ae2177b68f21aa190e24ef9
Darek Krawczyk <Darek9Krawczyk@hotmail.com>
2024-08-27 00:35:59 +0200

    Class library and tests

---
+++ SimulationTests/VectorTests.cs
@@ -0,0 +1,77 @@
+﻿namespace SimulationTests
+{
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void ScalarProduct()
+        {
+            Vector v1 = new Vector(1,1,1);
+            Vector v2 = new Vector(1,1,1);
+
+            double scalarProduct = v1 * v2;
+
+            Assert.IsTrue(scalarProduct == 3);
+        }
+
+        [TestMethod]
+        public void ScalarProductTimesScalar()
+        {
+            Vector v1 = new Vector(1, 1, 1);
+            double scalar = 5;
+
+            Vector result = v1 * 5;
+
+            Assert.IsTrue(result.X == 5 && result.Y == 5 && result.Z == 5);
+        }
+
+        [TestMethod]
+        public void Length()
+        {
+            Vector v1 = new Vector(1, 1, 1);
+
+            double length = v1.Length;
+
+            Assert.IsTrue(length == Math.Sqrt(1*1 + 1*1 + 1*1));
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            Vector v1 = new Vector(1, 1, 1);
+            Vector v2 = new Vector(1, 1, 1);
+
+            Vector result = v1 + v2;
+
+            Assert.IsTrue((result.X == v1.X + v2.X) &&
+                          (result.Y == v1.Y + v2.Y) &&
+                          (result.Z == v1.Z + v2.Z));
+        }
+
+        [TestMethod]
+        public void Subtract()
+        {
+            Vector v1 = new Vector(1, 1, 1);
+            Vector v2 = new Vector(1, 1, 1);
+
+            Vector result = v1 - v2;
+
+            Assert.IsTrue((result.X == v1.X - v2.X) &&
+                          (result.Y == v1.Y - v2.Y) &&
+                          (result.Z == v1.Z - v2.Z));
+        }
+
+        [TestMethod]
+        public void CrossProduct()
+        {
+            Vector v1 = new Vector(1, 1, 1);
+            Vector v2 = new Vector(1, 1, 1);
+
+            Vector result = v1 & v2;
+
+            Assert.IsTrue((result.X == v1.X - v2.X) &&
+                          (result.Y == v1.Y - v2.Y) &&
+                          (result.Z == v1.Z - v2.Z));
+        }
+    }
+}
---
+++ SimulationTypes/Vector.cs
@@ -0,0 +1,78 @@
+﻿public class Vector
+{
+    public double X;
+    public double Y;
+    public double Z;
+
+    public double Length
+    {
+        get
+        {
+            return Math.Sqrt(this * this);
+        }
+    }
+
+    public Vector()
+    {
+        X = 0; Y = 0; Z = 0;
+    }
+
+    public Vector(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public Vector(Vector other)
+    {
+        X = other.X;
+        Y = other.Y;
+        Z = other.Z;
+    }
+
+    public static double operator * (Vector v1, Vector v2)
+    {
+        return (v1.X * v2.X) + (v1.Y * v2.Y) + (v1.Z * v2.Z);
+    }
+
+    public static Vector operator *(Vector v1, double scalar)
+    {
+        return new Vector((v1.X * scalar), (v1.Y * scalar), (v1.Z * scalar));
+    }
+
+    public static Vector operator + (Vector v1, Vector v2)
+    {
+        return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+    }
+
+    public static Vector operator - (Vector v1, Vector v2)
+    {
+        return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+    }
+
+    /// <summary>
+    /// Cross product.
+    /// </summary>
+    /// <param name="v1">First Vector</param>
+    /// <param name="v2">Second Vector</param>
+    /// <returns>New object with cross product.</returns>
+    public static Vector operator & (Vector v1, Vector v2)
+    {
+        return new Vector(v1.Y * v2.Z - v1.Z * v2.Y,
+                          v1.Z * v2.X - v1.X * v2.Z,
+                          v1.X * v2.Y - v1.Y * v2.X);
+    }
+
+    public void Normalize()
+    {
+        double length = Length;
+        if(length != 1)
+        {
+            Vector result = this * (1 / length);
+            X = result.X; 
+            Y = result.Y; 
+            Z = result.Z;
+        }
+    }
+}
